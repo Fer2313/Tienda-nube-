45b7fe2e614d010c664d454e79d8d45e8aaf0d6a
Fer2313 <fernandojosefantini7.0@gmail.com>
2024-08-08 12:58:03 -0300

    primeros cambios

---
+++ Front-end/src/app/chackraStyles/styles.ts
@@ -0,0 +1,4 @@
+export const titleSize = { base: "18px", md: "26px", lg: "26px" };
+export const subtitleSize = { base: "16px", md: "20px", lg: "20px" };
+export const text = { base: "10px", md: "16px", lg: "16px" };
+export const input = { base: "md", md:"lg", lg: "lg" }
